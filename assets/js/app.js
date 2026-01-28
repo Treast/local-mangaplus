a50cdc63731358f5a73da9b0fca258f637b62e84
Vincent Riva <vincentriva@citia.org>
2026-01-28 06:30:26 +0100

    💄 Add some tooltips

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,12 @@
 import Alpine from "alpinejs";
+import tippy from "tippy.js";
 import "../bootstrap.js";
 
 import "../css/app.css";
+import "tippy.js/dist/tippy.css";
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+tippy("[data-tippy-content]");
